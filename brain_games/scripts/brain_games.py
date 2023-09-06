b3962fdd7ea85c8c23b9acae1fa3220e0f4d40bd
Evgeniy <Taleiran7@gmail.com>
2023-09-06 19:33:58 +0400

    modified brain_games/scripts/brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
+from ..cli import welcome_user
+
 
 def main():
     print("Welcome to the Brain Games!")
+
 
 if __name__ == "__main__":
     main()
 
 
-from ..cli import welcome_user
-
 def main():
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
